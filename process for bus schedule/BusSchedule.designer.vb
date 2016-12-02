e3e3b6041ddc9cfa2c1353b84800d164af9135dd
Welcyrie <wellnameless@yahoo.com>
2016-12-02 12:59:09 +0800

    Add files via upload
    1_Developers: [Wong Jun Xiang]
    2_Time: [02:51]
    3_Use_case: [User can view the bus schedule], [User able add new schedule for bus schedule] [User able to update the selected table bus schedule]

---
+++ process for bus schedule/BusSchedule.designer.vb
@@ -0,0 +1,383 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BusSchedule
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.bsDGV = New System.Windows.Forms.DataGridView()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.mskSeat = New System.Windows.Forms.MaskedTextBox()
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.calendar1 = New System.Windows.Forms.MonthCalendar()
+        Me.cboRoute = New System.Windows.Forms.ComboBox()
+        Me.mskTime = New System.Windows.Forms.MaskedTextBox()
+        Me.cboStaff = New System.Windows.Forms.ComboBox()
+        Me.cboBus = New System.Windows.Forms.ComboBox()
+        Me.lblBsID = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.err = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UpdateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.bsDGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.err, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.bsDGV)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(771, 261)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Bus Schedule List"
+        '
+        'bsDGV
+        '
+        Me.bsDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.bsDGV.Location = New System.Drawing.Point(9, 18)
+        Me.bsDGV.Name = "bsDGV"
+        Me.bsDGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.bsDGV.Size = New System.Drawing.Size(756, 230)
+        Me.bsDGV.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Enabled = False
+        Me.btnAdd.Location = New System.Drawing.Point(13, 227)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "&Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(123, 227)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(231, 227)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.mskSeat)
+        Me.GroupBox2.Controls.Add(Me.lblDate)
+        Me.GroupBox2.Controls.Add(Me.calendar1)
+        Me.GroupBox2.Controls.Add(Me.cboRoute)
+        Me.GroupBox2.Controls.Add(Me.mskTime)
+        Me.GroupBox2.Controls.Add(Me.cboStaff)
+        Me.GroupBox2.Controls.Add(Me.cboBus)
+        Me.GroupBox2.Controls.Add(Me.lblBsID)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.btnAdd)
+        Me.GroupBox2.Controls.Add(Me.btnExit)
+        Me.GroupBox2.Controls.Add(Me.btnClear)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 288)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(771, 258)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        '
+        'mskSeat
+        '
+        Me.mskSeat.Enabled = False
+        Me.mskSeat.Location = New System.Drawing.Point(110, 168)
+        Me.mskSeat.Mask = "00"
+        Me.mskSeat.Name = "mskSeat"
+        Me.mskSeat.Size = New System.Drawing.Size(121, 20)
+        Me.mskSeat.TabIndex = 25
+        '
+        'lblDate
+        '
+        Me.lblDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblDate.Location = New System.Drawing.Point(425, 11)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(121, 23)
+        Me.lblDate.TabIndex = 24
+        Me.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'calendar1
+        '
+        Me.calendar1.Location = New System.Drawing.Point(425, 43)
+        Me.calendar1.MaxSelectionCount = 1
+        Me.calendar1.Name = "calendar1"
+        Me.calendar1.ShowToday = False
+        Me.calendar1.ShowTodayCircle = False
+        Me.calendar1.TabIndex = 23
+        '
+        'cboRoute
+        '
+        Me.cboRoute.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboRoute.FormattingEnabled = True
+        Me.cboRoute.Location = New System.Drawing.Point(110, 47)
+        Me.cboRoute.Name = "cboRoute"
+        Me.cboRoute.Size = New System.Drawing.Size(121, 21)
+        Me.cboRoute.TabIndex = 18
+        '
+        'mskTime
+        '
+        Me.mskTime.Location = New System.Drawing.Point(597, 13)
+        Me.mskTime.Mask = "##:##"
+        Me.mskTime.Name = "mskTime"
+        Me.mskTime.Size = New System.Drawing.Size(121, 20)
+        Me.mskTime.TabIndex = 16
+        '
+        'cboStaff
+        '
+        Me.cboStaff.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboStaff.FormattingEnabled = True
+        Me.cboStaff.Location = New System.Drawing.Point(110, 127)
+        Me.cboStaff.Name = "cboStaff"
+        Me.cboStaff.Size = New System.Drawing.Size(121, 21)
+        Me.cboStaff.TabIndex = 14
+        '
+        'cboBus
+        '
+        Me.cboBus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboBus.FormattingEnabled = True
+        Me.cboBus.Location = New System.Drawing.Point(110, 88)
+        Me.cboBus.Name = "cboBus"
+        Me.cboBus.Size = New System.Drawing.Size(121, 21)
+        Me.cboBus.TabIndex = 13
+        '
+        'lblBsID
+        '
+        Me.lblBsID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblBsID.Location = New System.Drawing.Point(110, 11)
+        Me.lblBsID.Name = "lblBsID"
+        Me.lblBsID.Size = New System.Drawing.Size(121, 23)
+        Me.lblBsID.TabIndex = 11
+        Me.lblBsID.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(10, 171)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(38, 13)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Seat : "
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(552, 16)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Time : "
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(380, 16)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Date : "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(10, 130)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Driver Id : "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Bus Id : "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Route Id : "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Bus Schedule Id : "
+        '
+        'err
+        '
+        Me.err.ContainerControl = Me
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(786, 24)
+        Me.MenuStrip1.TabIndex = 5
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearToolStripMenuItem, Me.ToolStripMenuItem1, Me.ExitToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.MenuToolStripMenuItem.Text = "&File"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(143, 22)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(140, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(143, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AddToolStripMenuItem, Me.UpdateToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.U), System.Windows.Forms.Keys)
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AddToolStripMenuItem
+        '
+        Me.AddToolStripMenuItem.Name = "AddToolStripMenuItem"
+        Me.AddToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.AddToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.AddToolStripMenuItem.Text = "Add"
+        '
+        'UpdateToolStripMenuItem
+        '
+        Me.UpdateToolStripMenuItem.Name = "UpdateToolStripMenuItem"
+        Me.UpdateToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.U), System.Windows.Forms.Keys)
+        Me.UpdateToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.UpdateToolStripMenuItem.Text = "Update"
+        '
+        'BusSchedule
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(786, 549)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "BusSchedule"
+        Me.Text = "BusSchedule"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.bsDGV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.err, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents bsDGV As System.Windows.Forms.DataGridView
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents mskTime As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents cboStaff As System.Windows.Forms.ComboBox
+    Friend WithEvents cboBus As System.Windows.Forms.ComboBox
+    Friend WithEvents lblBsID As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cboRoute As System.Windows.Forms.ComboBox
+    Friend WithEvents err As System.Windows.Forms.ErrorProvider
+    Friend WithEvents lblDate As System.Windows.Forms.Label
+    Friend WithEvents calendar1 As System.Windows.Forms.MonthCalendar
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AddToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents UpdateToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mskSeat As System.Windows.Forms.MaskedTextBox
+End Class
