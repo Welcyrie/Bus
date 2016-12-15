c25e0b655343b8cd9ac297d0bbdbbab8d612fb2d
hockseng93 <hockseng9393@gmail.com>
2016-12-15 22:59:15 +0800

    finish[#136174791]
    update & delete code together

---
+++ StaffUpdate.Designer.vb
@@ -0,0 +1,401 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class StaffUpdate
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.label11 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.grbGender = New System.Windows.Forms.GroupBox()
+        Me.rbdFemale = New System.Windows.Forms.RadioButton()
+        Me.rbdMale = New System.Windows.Forms.RadioButton()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UpdateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mskPhNum = New System.Windows.Forms.MaskedTextBox()
+        Me.mskIC = New System.Windows.Forms.MaskedTextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.cboPosition = New System.Windows.Forms.ComboBox()
+        Me.cboStatus = New System.Windows.Forms.ComboBox()
+        Me.err = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.grbGender.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.err, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(291, 228)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(91, 49)
+        Me.btnExit.TabIndex = 12
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(290, 70)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(91, 49)
+        Me.btnUpdate.TabIndex = 10
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(291, 151)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(91, 49)
+        Me.btnDelete.TabIndex = 11
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'lblID
+        '
+        Me.lblID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblID.Location = New System.Drawing.Point(120, 35)
+        Me.lblID.Name = "lblID"
+        Me.lblID.Size = New System.Drawing.Size(157, 23)
+        Me.lblID.TabIndex = 0
+        '
+        'label11
+        '
+        Me.label11.AutoSize = True
+        Me.label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label11.Location = New System.Drawing.Point(65, 255)
+        Me.label11.Name = "label11"
+        Me.label11.Size = New System.Drawing.Size(48, 16)
+        Me.label11.TabIndex = 86
+        Me.label11.Text = "Status:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(54, 229)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(59, 16)
+        Me.Label8.TabIndex = 85
+        Me.Label8.Text = "Position:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(69, 177)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(45, 16)
+        Me.Label6.TabIndex = 84
+        Me.Label6.Text = "Email:"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(119, 177)
+        Me.txtEmail.MaxLength = 50
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(158, 20)
+        Me.txtEmail.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(39, 125)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 16)
+        Me.Label3.TabIndex = 83
+        Me.Label3.Text = "IC Number:"
+        '
+        'grbGender
+        '
+        Me.grbGender.Controls.Add(Me.rbdFemale)
+        Me.grbGender.Controls.Add(Me.rbdMale)
+        Me.grbGender.Location = New System.Drawing.Point(119, 87)
+        Me.grbGender.Name = "grbGender"
+        Me.grbGender.Size = New System.Drawing.Size(159, 32)
+        Me.grbGender.TabIndex = 73
+        Me.grbGender.TabStop = False
+        '
+        'rbdFemale
+        '
+        Me.rbdFemale.AutoSize = True
+        Me.rbdFemale.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbdFemale.Location = New System.Drawing.Point(81, 9)
+        Me.rbdFemale.Name = "rbdFemale"
+        Me.rbdFemale.Size = New System.Drawing.Size(72, 20)
+        Me.rbdFemale.TabIndex = 3
+        Me.rbdFemale.TabStop = True
+        Me.rbdFemale.Text = "Female"
+        Me.rbdFemale.UseVisualStyleBackColor = True
+        '
+        'rbdMale
+        '
+        Me.rbdMale.AutoSize = True
+        Me.rbdMale.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbdMale.Location = New System.Drawing.Point(6, 9)
+        Me.rbdMale.Name = "rbdMale"
+        Me.rbdMale.Size = New System.Drawing.Size(56, 20)
+        Me.rbdMale.TabIndex = 2
+        Me.rbdMale.TabStop = True
+        Me.rbdMale.Text = "Male"
+        Me.rbdMale.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(57, 96)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(56, 16)
+        Me.Label7.TabIndex = 82
+        Me.Label7.Text = "Gender:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(89, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(24, 16)
+        Me.Label1.TabIndex = 69
+        Me.Label1.Text = "ID:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(65, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 16)
+        Me.Label2.TabIndex = 71
+        Me.Label2.Text = "Name:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(43, 203)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(71, 16)
+        Me.Label5.TabIndex = 75
+        Me.Label5.Text = "Password:"
+        '
+        'txtPass
+        '
+        Me.txtPass.Location = New System.Drawing.Point(120, 203)
+        Me.txtPass.MaxLength = 20
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.Size = New System.Drawing.Size(158, 20)
+        Me.txtPass.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 151)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(101, 16)
+        Me.Label4.TabIndex = 74
+        Me.Label4.Text = "Phone Number:"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(393, 24)
+        Me.MenuStrip1.TabIndex = 87
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.UpdateToolStripMenuItem, Me.DeleteToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.M), System.Windows.Forms.Keys)
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.MenuToolStripMenuItem.Text = "&File"
+        '
+        'UpdateToolStripMenuItem
+        '
+        Me.UpdateToolStripMenuItem.Name = "UpdateToolStripMenuItem"
+        Me.UpdateToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.U), System.Windows.Forms.Keys)
+        Me.UpdateToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.UpdateToolStripMenuItem.Text = "&Update"
+        '
+        'DeleteToolStripMenuItem
+        '
+        Me.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem"
+        Me.DeleteToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.D), System.Windows.Forms.Keys)
+        Me.DeleteToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.DeleteToolStripMenuItem.Text = "&Delete"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(151, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'mskPhNum
+        '
+        Me.mskPhNum.Location = New System.Drawing.Point(119, 151)
+        Me.mskPhNum.Mask = "000-0000000"
+        Me.mskPhNum.Name = "mskPhNum"
+        Me.mskPhNum.Size = New System.Drawing.Size(158, 20)
+        Me.mskPhNum.TabIndex = 5
+        '
+        'mskIC
+        '
+        Me.mskIC.Location = New System.Drawing.Point(120, 125)
+        Me.mskIC.Mask = "000000-00-0000"
+        Me.mskIC.Name = "mskIC"
+        Me.mskIC.Size = New System.Drawing.Size(158, 20)
+        Me.mskIC.TabIndex = 4
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(120, 62)
+        Me.txtName.MaxLength = 40
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(158, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'cboPosition
+        '
+        Me.cboPosition.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboPosition.FormattingEnabled = True
+        Me.cboPosition.Items.AddRange(New Object() {"Driver", "Casher"})
+        Me.cboPosition.Location = New System.Drawing.Point(120, 229)
+        Me.cboPosition.Name = "cboPosition"
+        Me.cboPosition.Size = New System.Drawing.Size(158, 21)
+        Me.cboPosition.TabIndex = 8
+        '
+        'cboStatus
+        '
+        Me.cboStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboStatus.FormattingEnabled = True
+        Me.cboStatus.Items.AddRange(New Object() {"Available", "unavailable"})
+        Me.cboStatus.Location = New System.Drawing.Point(119, 256)
+        Me.cboStatus.Name = "cboStatus"
+        Me.cboStatus.Size = New System.Drawing.Size(158, 21)
+        Me.cboStatus.TabIndex = 9
+        '
+        'err
+        '
+        Me.err.ContainerControl = Me
+        '
+        'StaffUpdate
+        '
+        Me.AcceptButton = Me.btnUpdate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(393, 283)
+        Me.Controls.Add(Me.cboStatus)
+        Me.Controls.Add(Me.cboPosition)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.mskPhNum)
+        Me.Controls.Add(Me.mskIC)
+        Me.Controls.Add(Me.lblID)
+        Me.Controls.Add(Me.label11)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.grbGender)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "StaffUpdate"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Staff Details"
+        Me.grbGender.ResumeLayout(False)
+        Me.grbGender.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.err, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnUpdate As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents lblID As System.Windows.Forms.Label
+    Friend WithEvents label11 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents grbGender As System.Windows.Forms.GroupBox
+    Friend WithEvents rbdFemale As System.Windows.Forms.RadioButton
+    Friend WithEvents rbdMale As System.Windows.Forms.RadioButton
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtPass As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents UpdateToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DeleteToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mskPhNum As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents mskIC As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents cboPosition As System.Windows.Forms.ComboBox
+    Friend WithEvents cboStatus As System.Windows.Forms.ComboBox
+    Friend WithEvents err As System.Windows.Forms.ErrorProvider
+End Class
