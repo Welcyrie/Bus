0b2f8c131ce1bb8c96ddb5b686db4d815028c949
Welcyrie <wellnameless@yahoo.com>
2016-12-16 11:40:11 +0800

    Add files via upload

---
+++ process coding for bus schedule(1)/ViewBusInfo.designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ViewBusInfo
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.dgvBus = New System.Windows.Forms.DataGridView()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.dgvBus, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.dgvBus)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(460, 350)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Bus Information"
+        '
+        'dgvBus
+        '
+        Me.dgvBus.AllowUserToAddRows = False
+        Me.dgvBus.AllowUserToDeleteRows = False
+        Me.dgvBus.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvBus.Location = New System.Drawing.Point(6, 19)
+        Me.dgvBus.Name = "dgvBus"
+        Me.dgvBus.ReadOnly = True
+        Me.dgvBus.Size = New System.Drawing.Size(448, 325)
+        Me.dgvBus.TabIndex = 0
+        '
+        'btnClose
+        '
+        Me.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClose.Location = New System.Drawing.Point(205, 370)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 1
+        Me.btnClose.Text = "&Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'ViewBusInfo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnClose
+        Me.ClientSize = New System.Drawing.Size(485, 397)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "ViewBusInfo"
+        Me.Text = "Bus information"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.dgvBus, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents dgvBus As System.Windows.Forms.DataGridView
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+End Class
---
+++ process coding for bus schedule(1)/ViewStaffInfo.vb
@@ -0,0 +1,15 @@
+﻿Public Class ViewStaffInfo
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ViewStaffInfo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim db As New BusTicketingDataContext
+        Dim rs = From s In db.Staffs
+                 Select s.staffID, s.staffName, s.staffIC, s.staffGender, s.staffPhNum, s.staffEmail, s.staffPosition, s.staffStatus
+                 Where staffPosition = "Driver"
+        dgvStaff.DataSource = rs
+    End Sub
+
+End Class
