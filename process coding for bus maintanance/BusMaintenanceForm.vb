eaf17b40a91282823d784667cb4312790d890a6e
Welcyrie <wellnameless@yahoo.com>
2016-12-19 11:49:43 +0800

    Add files via upload

---
+++ process coding for bus maintanance/BusMaintenanceForm.vb
@@ -0,0 +1,152 @@
+﻿Public Class BusMaintenanceForm
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        If Me.ValidateChildren = False Then
+            Return
+        End If
+
+        Try
+            Dim db As New BusTicketingDataContext
+            Dim s As New bus
+            ' Dim BusID = lblBusId.Text
+
+            s.busID = lblBusId.Text
+            s.busPlateNum = maskNumber.Text
+            s.busSeatAmount = comboSeat.SelectedItem.ToString()
+            s.insuranceDate = CStr(Me.Calendar1.SelectionRange.Start)
+            s.busRoadTax = Decimal.Parse(txtRoad.Text)
+            s.busStatus = comboStatus.SelectedItem.ToString()
+
+            db.bus.InsertOnSubmit(s)
+            db.SubmitChanges()
+            'MessageBox.Show(lblBusId.Text & " Insert successful", "Complete", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ResetForm()
+
+            BindData()
+        Catch ex As Exception
+            MessageBox.Show("Please enter the required information", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub ResetForm()
+
+        maskNumber.Text = ""
+        comboSeat.SelectedIndex = -1
+        txtRoad.Text = ""
+        comboStatus.SelectedIndex = -1
+        generate_ID()
+
+    End Sub
+
+    Private Sub BindData()
+
+        Dim busId As String = lblBusId.Text
+
+
+        Dim db As New BusTicketingDataContext
+        Dim rs = From s In db.Bus
+
+        dgv.DataSource = rs
+
+    End Sub
+
+
+    Private Sub maskNumber_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles maskNumber.Validating
+
+        Dim number As String = If(maskNumber.MaskCompleted, maskNumber.Text, "")
+
+        If number = "" Then
+            err.SetError(maskNumber, "Please enter a valid " & number)
+            e.Cancel = True
+        Else
+            err.SetError(maskNumber, Nothing)
+        End If
+
+    End Sub
+
+    Private Sub txtRoad_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs)
+        If txtRoad.Text = "" Then
+            err.SetError(txtRoad, "Please enter the Amount of Road Tax")
+            e.Cancel = True
+        ElseIf IsNumeric(txtRoad.Text) Then
+            err.SetError(txtRoad, Nothing)
+        Else
+            err.SetError(txtRoad, "Please enter a numeric number only")
+            e.Cancel = True
+        End If
+    End Sub
+
+    Private Sub BusMaintenanceForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        generate_ID()
+        Calendar1.MinDate = Date.Now.ToShortDateString
+        BindData()
+
+    End Sub
+
+    Private Sub generate_ID()
+        Dim db As New BusTicketingDataContext
+        Dim rs = From a In db.bus
+
+        'Dim id = rs.Count.ToString("ST000")
+        Dim id As String = ""
+        For Each z In rs
+            id = z.busId
+        Next
+        Dim id1 As String = id.ToCharArray(0, 2)
+        Dim id2 As Integer = CInt(id.Substring(2))
+        id2 += 1
+        Dim id3 As String = CStr(id2)
+        For r = 0 To 2
+            If id3.Length <> 3 Then
+                id3 = "0" & id3
+            End If
+        Next
+
+        id = id1 & id3
+        lblBusId.Text = id
+        'lblID.Text = rs.Count.ToString("ST000")
+
+
+    End Sub
+
+    Private Sub dgv_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgv.CellDoubleClick
+        Dim i As Integer = e.RowIndex
+
+        If i > -1 Then
+            Dim id As String = CStr(dgv.Rows(i).Cells(0).Value)
+            UpdateForm.SelectedId = id
+            UpdateForm.ShowDialog(Me)
+
+            BindData()
+
+        End If
+    End Sub
+
+
+    Private Sub InsertToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles InsertToolStripMenuItem.Click
+        MessageBox.Show("Double click the data to update or delete", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtRoad_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtRoad.KeyPress
+        If Char.IsDigit(e.KeyChar) = False Then
+            If e.KeyChar = CChar(ChrW(Keys.Back)) Or e.KeyChar = CChar(ChrW(Keys.Space)) Then
+                e.Handled = False
+            Else
+                e.Handled = True
+                MessageBox.Show("You can only key in number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        End If
+    End Sub
+End Class
---
+++ process coding for bus maintanance/UpdateForm.designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UpdateForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtRoadTax = New System.Windows.Forms.TextBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.Calendar1 = New System.Windows.Forms.MonthCalendar()
+        Me.comboStatus = New System.Windows.Forms.ComboBox()
+        Me.comboSeat = New System.Windows.Forms.ComboBox()
+        Me.maskNumber = New System.Windows.Forms.MaskedTextBox()
+        Me.lblId = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.err = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.err, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtRoadTax)
+        Me.GroupBox1.Controls.Add(Me.btnExit)
+        Me.GroupBox1.Controls.Add(Me.btnDelete)
+        Me.GroupBox1.Controls.Add(Me.btnUpdate)
+        Me.GroupBox1.Controls.Add(Me.Calendar1)
+        Me.GroupBox1.Controls.Add(Me.comboStatus)
+        Me.GroupBox1.Controls.Add(Me.comboSeat)
+        Me.GroupBox1.Controls.Add(Me.maskNumber)
+        Me.GroupBox1.Controls.Add(Me.lblId)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(508, 507)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Bus Information"
+        '
+        'txtRoadTax
+        '
+        Me.txtRoadTax.Location = New System.Drawing.Point(210, 351)
+        Me.txtRoadTax.Name = "txtRoadTax"
+        Me.txtRoadTax.Size = New System.Drawing.Size(100, 20)
+        Me.txtRoadTax.TabIndex = 4
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(373, 456)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(87, 45)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "&Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(279, 456)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(87, 45)
+        Me.btnDelete.TabIndex = 7
+        Me.btnDelete.Text = "&Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(185, 456)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(87, 45)
+        Me.btnUpdate.TabIndex = 6
+        Me.btnUpdate.Text = "&Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'Calendar1
+        '
+        Me.Calendar1.Location = New System.Drawing.Point(210, 169)
+        Me.Calendar1.MaxSelectionCount = 1
+        Me.Calendar1.Name = "Calendar1"
+        Me.Calendar1.ShowToday = False
+        Me.Calendar1.ShowTodayCircle = False
+        Me.Calendar1.TabIndex = 3
+        '
+        'comboStatus
+        '
+        Me.comboStatus.FormattingEnabled = True
+        Me.comboStatus.Items.AddRange(New Object() {"On", "Off"})
+        Me.comboStatus.Location = New System.Drawing.Point(210, 399)
+        Me.comboStatus.Name = "comboStatus"
+        Me.comboStatus.Size = New System.Drawing.Size(121, 21)
+        Me.comboStatus.TabIndex = 5
+        '
+        'comboSeat
+        '
+        Me.comboSeat.FormattingEnabled = True
+        Me.comboSeat.Items.AddRange(New Object() {"45 ", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60"})
+        Me.comboSeat.Location = New System.Drawing.Point(210, 123)
+        Me.comboSeat.Name = "comboSeat"
+        Me.comboSeat.Size = New System.Drawing.Size(121, 21)
+        Me.comboSeat.TabIndex = 2
+        '
+        'maskNumber
+        '
+        Me.maskNumber.Location = New System.Drawing.Point(210, 83)
+        Me.maskNumber.Mask = "LLL0000"
+        Me.maskNumber.Name = "maskNumber"
+        Me.maskNumber.Size = New System.Drawing.Size(100, 20)
+        Me.maskNumber.TabIndex = 1
+        '
+        'lblId
+        '
+        Me.lblId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblId.Location = New System.Drawing.Point(210, 40)
+        Me.lblId.Name = "lblId"
+        Me.lblId.Size = New System.Drawing.Size(100, 23)
+        Me.lblId.TabIndex = 0
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(58, 399)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(43, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Status :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(58, 354)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(60, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Road Tax :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(41, 169)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(118, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Insurance Expire Date :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(41, 123)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Amount of Seat :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(41, 83)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Plate Number ;"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Bus ID :"
+        '
+        'err
+        '
+        Me.err.ContainerControl = Me
+        '
+        'UpdateForm
+        '
+        Me.AcceptButton = Me.btnUpdate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(532, 531)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "UpdateForm"
+        Me.Text = "Bus Maintenance"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.err, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnUpdate As System.Windows.Forms.Button
+    Friend WithEvents Calendar1 As System.Windows.Forms.MonthCalendar
+    Friend WithEvents comboStatus As System.Windows.Forms.ComboBox
+    Friend WithEvents comboSeat As System.Windows.Forms.ComboBox
+    Friend WithEvents maskNumber As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents lblId As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtRoadTax As System.Windows.Forms.TextBox
+    Friend WithEvents err As System.Windows.Forms.ErrorProvider
+End Class
