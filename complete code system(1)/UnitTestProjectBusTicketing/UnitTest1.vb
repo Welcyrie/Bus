6fc0e3ec6d7b8f8060101acce3f5fa16bd17ae05
Welcyrie <wellnameless@yahoo.com>
2016-12-20 11:56:02 +0800

    Add files via upload

---
+++ complete code system(1)/UnitTestProjectBusTicketing/UnitTest1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports NewVB
+
+<TestClass()> Public Class UnitTest1
+
+    '<TestMethod()> Public Sub TestMethod1()
+    'End Sub
+
+    <TestMethod()>
+    Public Sub TestDetailsView()
+        'Arrange
+        'Dim controller As New BusSchedule()
+        'Dim show1 As New ViewBusInfo()
+        'Dim show2 As New ViewRouteInfo
+        'Dim show3 As New ViewStaffInfo()
+
+        'Act
+
+
+        'Dim product As = controller.ViewData.Model
+        'Assert.AreEqual("Laptop", product.Name)
+        'Dim result As ViewResult = controller.Details(2)
+        'Assert.AreEqual("Details", result.ViewName)
+
+    End Sub
+
+
+End Class
